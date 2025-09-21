90b9c3fc1cc1d887f99e819de649cd20f4d05eec
SYED ABDUL RAHMAN <syedabdulrahman134@gmail.com>
2025-09-22 01:28:43 +0530

    Update ParcelController.java

--- src/main/java/com/example/hellomvc/controller/ParcelController.java
+++ src/main/java/com/example/hellomvc/controller/ParcelController.java
@@ -1,14 +1,13 @@
 package com.example.hellomvc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ParcelController {
 
-	@GetMapping("/parcel")
-	public String hello(Model model) {
-		return "here is a parcel";
-	}
+    @GetMapping("/parcel")
+    public String hello() {
+        return "here is a parcel";
+    }
 }
