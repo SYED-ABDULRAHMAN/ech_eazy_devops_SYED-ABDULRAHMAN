e205cca0b555e99d66fc64e63b738af95fda5c08
SYED ABDUL RAHMAN <syedabdulrahman134@gmail.com>
2025-09-22 00:02:34 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,59 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+  
+  validation {
+    condition = contains([
+      "t3.micro", "t3.small", "t3.medium", "t3.large",
+      "t2.micro", "t2.small", "t2.medium", "t2.large"
+    ], var.instance_type)
+    error_message = "Instance type must be a valid t2 or t3 instance type."
+  }
+}
+
+# Storage Configuration
+variable "root_volume_type" {
+  description = "Root volume type"
+  type        = string
+  default     = "gp3"
+}
+
+variable "root_volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+  default     = 20
+}
+
+# Key Pair Configuration
+variable "create_key_pair" {
+  description = "Whether to create a new key pair or use an existing one"
+  type        = bool
+  default     = true
+}
+
+variable "existing_key_name" {
+  description = "Name of existing key pair (used if create_key_pair is false)"
+  type        = string
+  default     = ""
+}
+
+# Application Configuration
+variable "github_repo_url" {
+  description = "GitHub repository URL for the application"
+  type        = string
+  default     = "https://github.com/Trainings-TechEazy/test-repo-for-devops.git"
+}
+
+variable "app_port" {
+  description = "Port on which the application will run"
+  type        = number
+  default     = 8080
+}
+
+# Elastic IP Configuration
+variable "create_elastic_ip" {
+  description = "Whether to create and assign an Elastic IP"
+  type        = bool
+  default     = false
+}
