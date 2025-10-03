237b827acebd280b0191f570484feb351934f286
SYED ABDUL RAHMAN <syedabdulrahman134@gmail.com>
2025-10-04 04:01:54 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -23,3 +23,26 @@ resource "aws_s3_bucket_lifecycle_configuration" "logs_lifecycle" {
     }
   }
 }
+# -----------------------------
+# Existing bucket (for logs) - already present
+# -----------------------------
+resource "aws_s3_bucket" "logs" {
+  bucket        = var.s3_bucket_name
+  force_destroy = true
+}
+
+# -----------------------------
+# New bucket for storing JAR file
+# -----------------------------
+resource "aws_s3_bucket" "app_jar_bucket" {
+  bucket        = var.app_jar_bucket_name
+  force_destroy = true
+}
+
+# -----------------------------
+# New bucket for ALB logs
+# -----------------------------
+resource "aws_s3_bucket" "elb_logs_bucket" {
+  bucket        = var.elb_logs_bucket_name
+  force_destroy = true
+}
