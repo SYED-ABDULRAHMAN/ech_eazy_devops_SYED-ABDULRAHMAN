8d5330328c75dfb720225fc29a976b060df0b7fd
SYED ABDUL RAHMAN <syedabdulrahman134@gmail.com>
2025-10-04 04:22:59 +0530

    Create autoscaling.tf

---
+++ terraform/autoscaling.tf
@@ -0,0 +1,13 @@
+resource "aws_autoscaling_group" "app_asg" {
+  desired_capacity     = var.instance_count
+  max_size             = var.instance_count
+  min_size             = var.instance_count
+  vpc_zone_identifier  = [aws_subnet.public1.id, aws_subnet.public2.id]
+
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+}
