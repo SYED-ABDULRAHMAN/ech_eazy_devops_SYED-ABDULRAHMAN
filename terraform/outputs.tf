bad9b20b170a77267c38aa38a7802cffa2cc8e5d
SYED ABDUL RAHMAN <syedabdulrahman134@gmail.com>
2025-10-04 16:08:26 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,6 @@ output "lb_dns_name" {
 }
 
 
-# SSH Connection
-output "ssh_connection_command" {
-  description = "Command to SSH into the instance"
-  value       = var.create_key_pair ? "ssh -i ${var.project_name}-key.pem ubuntu@${aws_instance.app_server.public_ip}" : "ssh -i YOUR_KEY.pem ubuntu@${aws_instance.app_server.public_ip}"
-}
-
 # Security Group
 output "security_group_id" {
   description = "ID of the security group"
@@ -22,12 +16,6 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main.id
 }
-
-output "subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
-}
-
 # Key Pair Information
 output "key_pair_name" {
   description = "Name of the key pair"
@@ -37,6 +25,15 @@ output "key_pair_name" {
 output "private_key_file" {
   description = "Path to the private key file (if created)"
   value       = var.create_key_pair ? "${var.project_name}-key.pem" : "Using existing key pair"
+}
+output "load_balancer_dns" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.app_lb.dns_name
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.app_tg.arn
 }
 
 
